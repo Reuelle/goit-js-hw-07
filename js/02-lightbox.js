46a8833d23a0ef29c263428226b1545ff7059ba7
Reuelle <ruel.paya@gmail.com>
2024-02-10 20:41:34 +0800

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,16 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const galleryContainer = document.querySelector("ul.gallery");
+
+function createGalleryItem(array) {
+    array.map(({preview, original, description}) => {
+        return `
+        <a class = "gallery__item" href="${original}>
+        <img class = "gallery__image" sec = "${preview}" alt = "${description} />
+        </a>
+        `;
+    }) .join("");
+
+}
+const photosMarkup = createGalleryItem(galleryItems);
