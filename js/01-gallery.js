753693c05e573c6ae8d794897266549234a583d9
Reuelle <ruel.paya@gmail.com>
2024-02-10 14:23:24 +0800

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -6,6 +6,7 @@ console.log(galleryItems);
 const gallery = document.querySelector(".gallery");
 
 galleryItems.forEach((item) => {
+    console.log(item);
 let li = `
 <li class="gallery__item">
   <a class="gallery__link" href="large-image.jpg">
