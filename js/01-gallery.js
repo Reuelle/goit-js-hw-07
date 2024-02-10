8b731682eac1e23d31d9851bbe6e24006860e604
Reuelle <ruel.paya@gmail.com>
2024-02-10 14:13:17 +0800

    task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,18 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+
+galleryItems.forEach((item) => {
+let li = '
+<li class ="gallery__item">
+<a class="gallery__link" href="large-image.jpg">
+  <img
+    class="gallery__image"
+    src="small-image.jpg"
+    data-source="large-image.jpg"
+    alt="Image description"
+  />
+</a> ';
+});
