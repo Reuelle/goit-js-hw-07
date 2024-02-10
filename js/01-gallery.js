490f6a0de8ff799ec1f169d68ffadc68d43d6ae1
Reuelle <ruel.paya@gmail.com>
2024-02-10 14:19:57 +0800

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -6,14 +6,16 @@ console.log(galleryItems);
 const gallery = document.querySelector(".gallery");
 
 galleryItems.forEach((item) => {
-let li = '
-<li class ="gallery__item">
-<a class="gallery__link" href="large-image.jpg">
-  <img
-    class="gallery__image"
-    src="small-image.jpg"
-    data-source="large-image.jpg"
-    alt="Image description"
-  />
-</a> ';
+let li = `
+<li class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src="small-image.jpg"
+      data-source="large-image.jpg"
+      alt="Image description"
+    />
+  </a>
+</li>
+`;
 });
