a003b9c327c14d39be670809f020147b8abc760d
Ruel <ruel.paya@gmail.com>
2024-02-11 09:04:44 +0800

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -8,8 +8,8 @@ const galleryContainer = document.querySelector("ul.gallery");
 function createGalleryItem(array) {
     return array.map(({preview, original, description}) => {
         return `
-        <a class = "gallery__item" href="${original}>
-        <img class = "gallery__image" src = "${preview}" alt = "${description} />
+        <a class="gallery__item" href="${original}>
+        <img class="gallery__image" src="${preview}" alt="${description} />
         </a>
         `;
     }) .join("");
